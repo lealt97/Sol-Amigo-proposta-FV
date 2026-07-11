7eba44ad7966c68ef4f662388025968246015d45
lealt97 <lealt97@gmail.com>
2026-07-10 23:25:20 -0300

    Reorganizar paginas internas do PDF

--- src/components/pdf/ProposalDocument.tsx
+++ src/components/pdf/ProposalDocument.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Document, Page, StyleSheet, Image } from '@react-pdf/renderer';
 import { Proposal } from '../../types/proposal';
 import { CoverPage } from './sections/CoverPage';
+import { IntroLetterSection } from './sections/IntroLetterSection';
 import { ExecutiveSummary } from './sections/ExecutiveSummary';
-import { TechnicalSection } from './sections/TechnicalSection';
+import { EnergyDiagnosisSection } from './sections/EnergyDiagnosisSection';
+import { SolarSolutionSection } from './sections/SolarSolutionSection';
+import { EquipmentSection } from './sections/EquipmentSection';
+import { GenerationSection } from './sections/GenerationSection';
 import { FinancialSection } from './sections/FinancialSection';
 import { PaybackSection } from './sections/PaybackSection';
 import { TermsSection } from './sections/TermsSection';
+import { WarrantyAndNextStepsSection } from './sections/WarrantyAndNextStepsSection';
 import { AcceptanceSection } from './sections/AcceptanceSection';
 
 const styles = StyleSheet.create({
@@ -31,6 +36,12 @@ interface ProposalDocumentProps {
   coverImage?: string | null;
 }
 
+const PageSection = ({ children }: { children: React.ReactNode }) => (
+  <Page size="A4" style={[styles.page, styles.section]}>
+    {children}
+  </Page>
+);
+
 export const ProposalDocument: React.FC<ProposalDocumentProps> = ({ proposal, coverImage }) => {
   return (
     <Document>
@@ -41,24 +52,47 @@ export const ProposalDocument: React.FC<ProposalDocumentProps> = ({ proposal, co
           <CoverPage proposal={proposal} />
         )}
       </Page>
-      
-      <Page size="A4" style={[styles.page, styles.section]}>
+
+      <PageSection>
+        <IntroLetterSection proposal={proposal} />
+      </PageSection>
+
+      <PageSection>
         <ExecutiveSummary proposal={proposal} />
-      </Page>
-      
-      <Page size="A4" style={[styles.page, styles.section]}>
-        <TechnicalSection proposal={proposal} />
-      </Page>
-      
-      <Page size="A4" style={[styles.page, styles.section]}>
+      </PageSection>
+
+      <PageSection>
+        <EnergyDiagnosisSection proposal={proposal} />
+      </PageSection>
+
+      <PageSection>
+        <SolarSolutionSection proposal={proposal} />
+      </PageSection>
+
+      <PageSection>
+        <EquipmentSection proposal={proposal} />
+      </PageSection>
+
+      <PageSection>
+        <GenerationSection proposal={proposal} />
+      </PageSection>
+
+      <PageSection>
         <FinancialSection proposal={proposal} />
+      </PageSection>
+
+      <PageSection>
         <PaybackSection proposal={proposal} />
-      </Page>
-      
-      <Page size="A4" style={[styles.page, styles.section]}>
+      </PageSection>
+
+      <PageSection>
         <TermsSection proposal={proposal} />
+      </PageSection>
+
+      <PageSection>
+        <WarrantyAndNextStepsSection proposal={proposal} />
         <AcceptanceSection proposal={proposal} />
-      </Page>
+      </PageSection>
     </Document>
   );
 };
